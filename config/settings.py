054c75977ed8ee923a4d06a492c08885472c39a6
sub2pewds12 <leanhkhoi1010@gmail.com>
2026-04-11 17:03:38 +0700

    Fix: Switch to SMTP Port 465 (SSL) to resolve network reachability issues

--- config/settings.py
+++ config/settings.py
@@ -147,15 +147,17 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email Configuration (Gmail)
+# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_TIMEOUT = 5
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # Auth Redirects
 LOGIN_URL = 'login'
